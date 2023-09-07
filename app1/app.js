dd0e3e87a91d3326d19739d4041b8fd0664919e1
Anasraza1212 <143824189+Anasraza1212@users.noreply.github.com>
2023-09-07 20:35:05 +0500

    Add files via upload

---
+++ app1/app.js
@@ -0,0 +1,61 @@
+            // CHAPTER#1
+alert('ANAS')
+alert(' RAZA!')
+alert('033327112821')
+alert('anas@gmail.com')
+alert('12345')
+alert('You are learning JS;')
+alert('Currently learing JAVA SCRIPT')
+
+//         // CHAPTER#2
+var name="anasRaza"
+var res=''
+var res='10'
+var team='PAKISTAN'
+alert(team)
+console.log('CHAPTER_2')
+var res='SHAHEEN Is The Best Bowler'
+console.log(res)
+document.write('ALL THE WORK IS IN CONSOLE;')
+
+
+        //  CHAPTER#3
+var caseqty=144
+var num=9
+console.log('CHAPTER_3')
+console.log(caseqty + num)
+var shipping=100
+var product=200
+console.log(shipping + product)
+        
+        //  CHAPTER#4
+var product_cost=3.45
+var name_of_brand='AUDI'
+console.log('CHAPTER_4')
+console.log(name_of_brand)
+var company=100000
+var firstname='anas'
+var lastname='Raza'
+console.log(firstname + lastname)
+console.log('LIST OF LEGAL VARIABLE;')
+var legal='Uppercase Lowercase number underscore dollar sign'
+console.log(legal)
+console.log('LIST OF ILLEGAL VARIABLE;')
+var illegal='if else var function'
+console.log(illegal)
+
+        // chapter#5
+console.log('CHAPTER_5')
+console.log('MODULUS and %')
+console.log('NUM = 2')
+var largenum=1000*2000
+console.log(largenum)
+var num1='287'
+var  num2='156'
+console.log(num1 - num2)
+var diviser=153
+var divisible=7
+console.log(diviser % divisible)
+var multiply1=7
+var multiply2=9
+alert(multiply1 * multiply2)
