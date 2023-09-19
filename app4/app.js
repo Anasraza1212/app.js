af9cee1ee14d4185798d2559e49d868685e01a71
Anasraza1212 <143824189+Anasraza1212@users.noreply.github.com>
2023-09-19 15:57:05 +0500

    Add files via upload

---
+++ app4/app.js
@@ -0,0 +1,73 @@
+// *************************HOME ASSIGNMENT********************************  
+                //    QUESTION#1
+for(i=1;i<=10;i++){console.log('WELCOME HOME')}
+
+                //    QUESTION#2
+for(i=1;i<=12;i++){console.log('GOOD EVENING')}
+
+                //    QUESTION#3
+"1:ROUND BRACKETS"
+"2:TWO SEMI-COLON"
+"3:CURLY BRACKETS"
+"4:INITIALIZATION"
+
+
+                //    QUESTION#4
+for(a=1;a<101;a++){console.log('welcome')}
+
+
+                //    QUESTION#5
+for(i=3;i<4;i--){console.log(i)}
+
+
+                //    QUESTION#6
+var num=[true,'anas',21,undefined]
+
+
+                //    QUESTION#7
+var flag=true
+
+
+                //    QUESTION#8
+var pet=['cat','dog','camel','tiger','lion']
+for(i=1;i<=pet.length;i++){console.log(pet)}
+
+
+
+                //    QUESTION#9
+var num=[1,2,3,4,5,6,7,8,9,10]
+for(i=1;i=num.length;i++){console.log(i),alert(num=[0])}
+alert(num=[1])
+
+
+                //    QUESTION#10
+var names=[username,username1,username2,username3,username4]
+var username=prompt('ENTER YOUR NAME')
+var username1=prompt('ENTER YOUR NAME')
+var username2=prompt('ENTER YOUR NAME')
+var username3=prompt('ENTER YOUR NAME')
+var username4=prompt('ENTER YOUR NAME')
+
+for(i=1;i=names.length ;i++){if(username=''){console.log('ENTER YOUR NAME USER')}
+if(username1=''){console.log('ENTER YOUR NAME USER1')}
+if(username2=''){console.log('ENTER YOUR NAME USER2')}
+if(username3=''){console.log('ENTER YOUR NAME USER3')}
+if(username4=''){console.log('ENTER YOUR NAME USER4')}}
+
+
+                //    QUESTION#11
+ var matchFound = false;
+ for (var i = 0; i < list.length; i++) {
+ if (userInput === list[i]) {
+ alert("Match found")
+ }
+ else(alert("Match not found")
+ )
+ }
+                
+
+                //    QUESTION#12
+var arr1=['a','b','c','d','e']
+var arr2=[1,2,3,4,5,6]
+var c=arr1.concat(arr2)
+for(i=1;i<=c.length;i++){console.log(c)}
