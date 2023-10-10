0b6ec4b34c976d65cb91cf2b9ec9f3bfd118701a
Anasraza1212 <143824189+Anasraza1212@users.noreply.github.com>
2023-10-10 16:51:18 +0500

    Add files via upload

---
+++ app7/app.js
@@ -0,0 +1,27 @@
+// <!-- *************************CHAPTER# 46 & 48 ( EVENTS ) *********************** -->
+
+// <!-- *****************************QUESTION#1******************* -->
+
+function button(){
+    alert('welcome to home page')
+}
+
+button()
+
+
+// <!-- *****************************QUESTION#2******************* -->
+
+function disappear(){car.style.display= 'none'}
+disappear()
+
+
+
+/* <!-- *******************CHAPTER# 49 & 50 ( Reading and Setting
+
+    field values ) ******************* -->*/
+
+// <!-- *****************************QUESTION#1******************* -->
+// var input=prompt('enter your name ')
+// console.log(input)
+
+console.log(document.getElementById(nam.value))
